8a20308240cb3c19ee25c85ac2b740ce0fc19429
cbello92 <camilo.bello92@gmail.com>
2021-10-17 23:37:49 -0300

    initial commit

---
+++ ApiGateway/Aggregators/UsersPostsAggregator.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Ocelot.Middleware;
+using Ocelot.Multiplexer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiGateway.Aggregators
+{
+    public class UsersPostsAggregator : IDefinedAggregator
+    {
+        public Task<DownstreamResponse> Aggregate(List<HttpContext> responses)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
