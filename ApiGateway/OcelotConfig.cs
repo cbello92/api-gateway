70d70cc284d75ca86a86a7f8b3867ab302cd851a
cbello92 <camilo.bello92@gmail.com>
2021-11-01 23:38:06 -0300

    configure routes ocelot

---
+++ ApiGateway/OcelotConfig.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace ApiGateway
+{
+    
+    public class OcelotConfig
+    {
+        public static T Clone<T>(T source)
+        {
+            var serialized = JsonConvert.SerializeObject(source);
+            return JsonConvert.DeserializeObject<T>(serialized);
+        }
+
+        public dynamic BuildRoutesConcepcion(dynamic baseJSON)
+        {
+
+            dynamic routesConcepcion = Clone(baseJSON);
+
+            foreach (var item in routesConcepcion)
+            {
+                if(item["UpstreamPathTemplate"].ToString().Contains("base"))
+                {
+                    item["UpstreamPathTemplate"] = item["UpstreamPathTemplate"].ToString().Replace("base", "concepcion");
+                    item["DownstreamHostAndPorts"][0]["Host"] = "localhost";
+                    item["DownstreamHostAndPorts"][0]["Port"] = 5002;
+                }
+            }
+
+            return routesConcepcion;
+        }
+
+
+        public dynamic BuildRoutesChillan(dynamic baseJSON)
+        {
+
+            dynamic routesChillan = Clone(baseJSON);
+
+            foreach (var item in routesChillan)
+            {
+                if (item["UpstreamPathTemplate"].ToString().Contains("base"))
+                {
+                    item["UpstreamPathTemplate"] = item["UpstreamPathTemplate"].ToString().Replace("base", "chillan");
+                    item["DownstreamHostAndPorts"][0]["Host"] = "chillanapi.cl";
+                    item["DownstreamHostAndPorts"][0]["Port"] = 80;
+                }
+            }
+
+            return routesChillan;
+        }
+
+
+        public List<dynamic> ConsolidateRoutes(dynamic baseJSON)
+        {
+            List<dynamic> routesConcat = new List<dynamic>();
+            dynamic routesConcepcion = BuildRoutesConcepcion(baseJSON);
+            dynamic routesChillan = BuildRoutesChillan(baseJSON);
+
+
+            foreach (var item in routesConcepcion)
+            {
+                if(routesConcat.FindAll(x => x["UpstreamPathTemplate"] == item["UpstreamPathTemplate"]).Count == 0)
+                {
+                    routesConcat.Add(item);
+                }
+            }
+
+            foreach (var item in routesChillan)
+            {
+                if (routesConcat.FindAll(x => x["UpstreamPathTemplate"] == item["UpstreamPathTemplate"]).Count == 0)
+                {
+                    routesConcat.Add(item);
+                }
+            }
+
+            return routesConcat;
+        }
+
+    }
+}
--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -2,9 +2,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace ApiGateway
@@ -13,6 +16,37 @@ namespace ApiGateway
     {
         public static void Main(string[] args)
         {
+            StreamReader r = new StreamReader("base.json");
+            string jsonString = r.ReadToEnd();
+
+            dynamic jsonRoutes = JsonConvert.DeserializeObject(jsonString);
+            OcelotConfig ocelotConfig = new OcelotConfig();
+
+            //dynamic routesConcepcion = ocelotConfig.BuildRoutesConcepcion(jsonRoutes);
+            //dynamic routesChillan = ocelotConfig.BuildRoutesChillan(jsonRoutes);
+
+            //List<dynamic> routesConcat = new List<dynamic>();
+
+            var ocelotObject = new
+            {
+                Routes = ocelotConfig.ConsolidateRoutes(jsonRoutes),
+                GlobalConfiguration = new
+                {
+                    BaseUrl = "https://localhost:5000"
+                } 
+            };
+
+            //foreach (var item in ocelotConfig.ConsolidateRoutes(jsonRoutes))
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //Console.WriteLine("FROM PROGRAM");
+
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string json = JsonConvert.SerializeObject(ocelotObject, Formatting.Indented);
+            File.WriteAllText(@"C:\Users\camil\Desktop\ApiGateway\ApiGateway\ocelot.json", json);
+
             CreateHostBuilder(args).Build().Run();
         }
 
